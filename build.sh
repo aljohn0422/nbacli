38cc150d908d9caf9394e01a8842e456762e83df
hannchyun <aljohn0422@gmail.com>
2021-01-28 15:54:47 +0800

    edit build script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-env GOOS=windows GOARCH=amd64 go build -o nba 
-zip windows-amd64.zip nba
+env GOOS=windows GOARCH=amd64 go build -o nba.exe
+zip windows-amd64.zip nba.exe
 env GOOS=darwin GOARCH=amd64 go build -o nba 
 zip mac-amd64.zip nba
 env GOOS=linux GOARCH=amd64 go build -o nba 
